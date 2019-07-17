5c9bd85628efa04a827ed0e55e232ac37e3c14e8
Edison <edison.mak@xenyo.com>
2019-07-17 12:57:45 +0800

    ajax post

---
+++ 0716/data.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by beebb.
+ * Date: 17/7/2019
+ * Time: 10:05
+ */
+
+echo 'Return by Ajax'.'<br>'.
+	 'Welcome , ' . $_POST['username'] . '<br>' .
+     'Your Email is :' . $_POST['email'] . '<br>' .
+     'Your Password is :' . $_POST['password'];
+
