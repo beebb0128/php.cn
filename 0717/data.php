dd2837e1562af161fc7fb9e1a21214926fceef21
Edison <edison.mak@xenyo.com>
2019-07-18 15:13:51 +0800

    0717 update

--- 0717/data.php
+++ 0717/data.php
@@ -5,3 +5,22 @@
  * Time: 10:05
  */
 
+
+$userData = [
+	'username' => 'admin',
+	'password' => '123456',
+	'email'    => 'admin@gmail.com'
+];
+
+//transfer array to json
+$jsonPost = json_encode( $_POST );
+echo $jsonPost.'<br>';
+
+//if($_POST['username'] == $userData['username']){
+//	echo '1';
+//}else{
+//	echo '2';
+//}
+
+//echo $jsonFormat;
+
